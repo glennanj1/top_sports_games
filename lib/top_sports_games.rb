e32f2aec9bd15b133362bb57cf73b53362de646d
john_glennan <glennan20@gmail.com>
2020-09-13 13:43:54 -0400

    added cli functionality

--- lib/top_sports_games.rb
+++ lib/top_sports_games.rb
@@ -1,8 +1,5 @@
 require_relative "./top_sports_games/version"
-
 require_relative './top_sports_games/cli'
 
-module TopSportsGames
-  class Error < StandardError; end
-  # Your code goes here...
-end
+
+
--- lib/top_sports_games/cli.rb
+++ lib/top_sports_games/cli.rb
@@ -1,8 +1,36 @@
 class TopSportsGames::CLI
-    
 
-    def call
-        puts "Today's Top Sports Games:"
+
+    def call 
+        list_games
+        menu
+        goodbye
     end
+
+    def list_games
+        puts "Today's Top Sports Games:"
+        puts "Games 1-10"
+    end
+
+    def menu
+        input = nil
+        while input != "exit"
+            puts "Enter the number of the game you would like more information on or type exit"
+            input = gets.strip.downcase
+            case input
+            when "1"
+                puts "info on 1"
+            when "2"
+                puts "info on 2"
+            when "3"
+                puts "info on 3"
+            end
+        end
+    end
+
+
+    def goodbye
+        puts "See you tomorrow for more games."
+    end 
 
 end
