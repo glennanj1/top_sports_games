b7fb3b0ba42169b4e34a017fe7de1a7bd3463204
john_glennan <glennan20@gmail.com>
2020-09-14 00:08:02 -0400

    finalizing project

--- lib/top_sports_games/cli.rb
+++ lib/top_sports_games/cli.rb
@@ -2,6 +2,8 @@ class TopSportsGames::CLI
 
 
     def call 
+        puts "Welcome to the Top Sports Games CLI!"
+        TopSportsGames::Game.scrape_site
         list_games
         menu
         goodbye
@@ -11,7 +13,8 @@ class TopSportsGames::CLI
         puts "Today's Top Sports Games:"
         @games = TopSportsGames::Game.today
         @games.each.with_index(1) do |game, i|
-            puts "#{i}, #{game.name}"
+            puts "#{i}, #{game.away} vs. #{game.home} at #{game.date}" 
+            
         end
     end
 
@@ -19,19 +22,39 @@ class TopSportsGames::CLI
         input = nil
         while input != "exit"
             puts "Enter the number of the game you would like more information on or type exit"
-            input = gets.strip.downcase
-
-            if input.to_i > 0 
-                the_game = @games[input.to_i-1]
-                puts "#{game.name}"
-            elsif input == "list"
+            input = gets.chomp
+            if !input.to_i.between?(1, Game.all.count)
+                puts "Game not found. Please select a different game!"
                 list_games
+                menu
             else
-                puts "Not sure what you want, type list or exit."
+                game = Game.all[input.to_i-1]
             end
+            puts "Would you like to see another game?"
+            puts "Please enter Y or N"
+            if another_game == "y"
+                list_games
+                menu
+            else
+                puts "Please try again."
+                list_games
+                menu
+            end
+
         end
+
+
     end
-            
+        
+    
+    def display_game_details(game)
+        Game.scrape_sit(game)
+        puts "Here are the details for #{character.name}:"
+        puts "#{game.date}"
+        puts "#{game.home}"
+        puts "#{game.away}"
+        puts "#{game.total}"
+    end
 
 
     def goodbye
--- lib/top_sports_games/game.rb
+++ lib/top_sports_games/game.rb
@@ -1,5 +1,5 @@
 class TopSportsGames::Game 
-    attr_accessor :name, :total, :date, :url
+    attr_accessor :away, :home, :total, :date, :url
 
     def self.today
         # Scrape Odds Portal return games and subsequent info
@@ -23,12 +23,18 @@ class TopSportsGames::Game
 
     def self.scrape_site
         doc = Nokogiri::HTML(open("https://www.oddsshark.com/nfl/computer-picks"))
-        
-        
-    
-        
-        
         binding.pry
+
+        
+
+
+        game = self.new
+        game.away = doc.css('span.name.table__name-away.tooltip').text
+        game.home = doc.css('span.name.table__name-home.tooltip').text
+        game.total = doc.css('td.pick-computer-pick-total-label.cell-a').text
+        game.date = doc.css('div.pick-date').text
+        game
+
     end
 
 end
